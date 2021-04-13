88c500024a720b81f65867fc9208183350b512bf
Alexandre Podlewski <alexandre.podlewski@fabernovel.com>
2021-04-13 14:25:42 +0200

    Create a xcodeproj for CI

---
+++ CompositionalLayoutDSLApp/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CompositionalLayoutDSL
+//
+//  Created by Alexandre Podlewski on 13/04/2021.
+//  Copyright © 2021 Fabernovel. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication,
+                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
