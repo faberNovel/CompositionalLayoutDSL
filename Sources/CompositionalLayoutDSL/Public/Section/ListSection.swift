41531a03a9c512fdefa53cd2bad676a4c5f253c5
Alexandre Podlewski <alexandre.podlewski@fabernovel.com>
2021-04-20 18:01:21 +0200

    Improve API for ListSection configuration

--- Sources/CompositionalLayoutDSL/Public/Section/ListSection.swift
+++ Sources/CompositionalLayoutDSL/Public/Section/ListSection.swift
@@ -11,7 +11,7 @@ import UIKit
 @available(iOS 14.0, tvOS 14, *)
 public struct ListSection: LayoutSection {
 
-    private let configuration: UICollectionLayoutListConfiguration
+    private var configuration: UICollectionLayoutListConfiguration
     private let layoutEnvironment: NSCollectionLayoutEnvironment
 
     // MARK: - Life cycle
@@ -23,6 +23,54 @@ public struct ListSection: LayoutSection {
     ) {
         self.configuration = configuration
         self.layoutEnvironment = layoutEnvironment
+    }
+
+    /// Creates a list section with the specified list appearance and layout environment.
+    public init(
+        appearance: UICollectionLayoutListConfiguration.Appearance,
+        layoutEnvironment: NSCollectionLayoutEnvironment
+    ) {
+        self.configuration = UICollectionLayoutListConfiguration(appearance: appearance)
+        self.layoutEnvironment = layoutEnvironment
+    }
+
+    // MARK: - ListSection
+
+    /// A Boolean value that determines whether the list shows separators between cells.
+    @available(tvOS, unavailable)
+    public func showsSeparators(_ showsSeparators: Bool) -> Self {
+        with(self) { $0.configuration.showsSeparators = showsSeparators }
+    }
+
+    /// The background color of the list.
+    ///
+    /// The default value is nil, which means that the configuration uses the system background
+    /// color for the specified appearance.
+    public func backgroundColor(_ backgroundColor: UIColor?) -> Self {
+        with(self) { $0.configuration.backgroundColor = backgroundColor }
+    }
+
+    @available(tvOS, unavailable)
+    public func trailingSwipeActionsConfigurationProvider(
+        // swiftlint:disable:next line_length
+        _ trailingSwipeActionsConfigurationProvider: UICollectionLayoutListConfiguration.SwipeActionsConfigurationProvider?
+    ) -> Self {
+        // swiftlint:disable:next line_length
+        with(self) { $0.configuration.trailingSwipeActionsConfigurationProvider = trailingSwipeActionsConfigurationProvider }
+    }
+
+    /// The type of header to use for the list.
+    ///
+    /// The default value is `UICollectionLayoutListConfiguration.HeaderMode.none`.
+    public func headerMode(_ headerMode: UICollectionLayoutListConfiguration.HeaderMode) -> Self {
+        with(self) { $0.configuration.headerMode = headerMode }
+    }
+
+    /// The type of footer to use for the list.
+    ///
+    /// The default value is `UICollectionLayoutListConfiguration.FooterMode.none`.
+    public func footerMode(_ footerMode: UICollectionLayoutListConfiguration.FooterMode) -> Self {
+        with(self) { $0.configuration.footerMode = footerMode }
     }
 
     // MARK: - LayoutSection
