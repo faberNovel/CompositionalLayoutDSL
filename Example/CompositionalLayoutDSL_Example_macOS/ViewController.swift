133b32042a669a1499bd0592f3cb363b286b55b9
Alexandre Podlewski <alexandre.podlewski@fabernovel.com>
2021-04-21 12:35:21 +0200

    Create macOS example target

---
+++ Example/CompositionalLayoutDSL_Example_macOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CompositionalLayoutDSL_Example_macOS
+//
+//  Created by Alexandre Podlewski on 21/04/2021.
+//  Copyright © 2021 Fabernovel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
